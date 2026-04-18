c10633f1d90b6156a600ad8bf022fe76d4def79e
yeowkenggg <yeowkc@stud.ntnu.no>
2026-04-18 20:22:46 +0200

    fix: render powerup fish at correct aspect ratio (fixed height 1.5f, width scaled)

--- core/src/main/java/com/tdt4240Grp04/clashofclaws/states/PlayView.java
+++ core/src/main/java/com/tdt4240Grp04/clashofclaws/states/PlayView.java
@@ -234,8 +234,9 @@ public class PlayView {
                     case 3:  tex = magnetTexture; break;
                     default: continue;
                 }
-                float s = 1.5f;
-                batch.draw(tex, sp.x - s / 2, sp.y - s / 2, s, s);
+                float h = 1.5f;
+                float w = h * (float) tex.getWidth() / tex.getHeight();
+                batch.draw(tex, sp.x - w / 2, sp.y - h / 2, w, h);
             }
         }
         batch.end();
